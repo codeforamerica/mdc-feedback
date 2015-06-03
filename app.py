41911bec662dc1b82e35239f55ffacff432743eb
Ernie Hsiung <ehsiung@codeforamerica.org>
2015-06-03 11:21:25 -0700

    Adding DB stuff

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import Survey
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
