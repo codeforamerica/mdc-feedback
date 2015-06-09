7b37fbc3d43bbac36e1bdcaab0a3d206c2f9a191
Ernie Hsiung <ehsiung@codeforamerica.org>
2015-06-09 10:11:59 -0700

    Let's add a UI

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import requests
+import requests_cache
+import time
+
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
@@ -6,16 +10,24 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
+requests_cache.install_cache(cache_name='github_cache', backend='sqlite', expire_after=180)
+
 from models import Survey
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+
+    hardcoded_api = 'https://api.typeform.com/v0/form/UYZYtI?key=433dcf9fb24804b47666bf62f83d25dbef2f629d&completed=true'
+    if request.method == 'POST':
+        print('POST method')
+        now = time.ctime(int(time.time()))
+        response = requests.get(hardcoded_api)
+        print "Time: {0} / Used Cache: {1}".format(now, response.from_cache)
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+    return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
     app.run()
