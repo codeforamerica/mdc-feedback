d345fe68b436f4037261b6c7ba5aa8e6f68f8292
Ernie Hsiung <ehsiung@codeforamerica.org>
2015-06-11 16:29:33 -0700

    Displaying table rows

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
-import requests_cache
 import time
 
 from flask import Flask, render_template, request
@@ -10,8 +9,6 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-requests_cache.install_cache(cache_name='github_cache', backend='sqlite', expire_after=180)
-
 from models import Survey
 
 @app.route('/', methods=['GET', 'POST'])
@@ -19,13 +16,57 @@ def index():
     errors = []
     results = {}
 
+    results = Survey.query.all()
+    print(results)
+
+
     hardcoded_api = 'https://api.typeform.com/v0/form/UYZYtI?key=433dcf9fb24804b47666bf62f83d25dbef2f629d&completed=true'
     if request.method == 'POST':
         print('POST method')
         now = time.ctime(int(time.time()))
-        response = requests.get(hardcoded_api)
-        print "Time: {0} / Used Cache: {1}".format(now, response.from_cache)
+        r = requests.get(hardcoded_api)
+        r_json = r.json()
 
+
+        for i in r_json['responses']:
+
+            try:
+                # TO DO: Add lang to models.py
+
+                '''
+                quiz_id=i['id'],
+                list_choice_1=i['answers']['list_7649733_choice'],
+                list_choice_2=i['answers']['list_7205097_choice'],
+                textfield_1=i['answers']['textfield_7205223'],
+                textarea_1=i['answers']['textarea_7205107'],
+                textarea_2=i['answers']['textarea_7205102'],
+                opinionscale_1=i['answers']['opinionscale_7205022']
+
+                print "start"
+                print "{0}, {1}, {2}, {3}, {4}, {5}, {6}".format(quiz_id, list_choice_1, list_choice_2, textfield_1, textarea_1, textarea_2, opinionscale_1)
+                print "end"
+                '''
+
+                survey = Survey(
+                    lang='en',
+                    quiz_id=i['id'],
+                    list_choice_1=i['answers']['list_7649733_choice'],
+                    list_choice_2=i['answers']['list_7205097_choice'],
+                    textfield_1=i['answers']['textfield_7205223'],
+                    textarea_1=i['answers']['textarea_7205107'],
+                    textarea_2=i['answers']['textarea_7205102'],
+                    opinionscale_1=i['answers']['opinionscale_7205022']
+                )
+                db.session.add(survey)
+                db.session.commit()
+
+                print "[row committed]"
+            except:
+                print "[row exception]"
+                errors.append("Unable to add item to database.")
+
+            # for j in i['answers']:
+            #     print j
 
     return render_template('index.html', errors=errors, results=results)
 
--- models.py
+++ models.py
@@ -2,17 +2,32 @@ import datetime
 from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
+# https://pythonhosted.org/Flask-SQLAlchemy/models.html
+
 class Survey(db.Model):
     __tablename__ = 'surveys'
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
-    survey_type = db.Column(db.String(255), nullable=False)
-    q1 = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, survey_type, q1):
-        self.survey_type = survey_type
-        self.q1 = q1
+    lang = db.Column(db.String(2), nullable=False)
+    quiz_id = db.Column(db.Integer)
+    list_choice_1 = db.Column(db.String(255))
+    list_choice_2 = db.Column(db.String(255))
+    textfield_1 = db.Column(db.String(255))
+    textarea_1 = db.Column(db.Text)
+    textarea_2 = db.Column(db.Text)
+    opinionscale_1 = db.Column(db.Integer)
+
+    def __init__(self, lang, quiz_id, list_choice_1, list_choice_2, textfield_1, textarea_1, textarea_2, opinionscale_1):
+        self.lang = lang
+        self.quiz_id = quiz_id
+        self.list_choice_1 = list_choice_1
+        self.list_choice_2 = list_choice_2
+        self.textfield_1 = textfield_1
+        self.textarea_1 = textarea_1
+        self.textarea_2 = textarea_2
+        self.opinionscale_1 = opinionscale_1
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
