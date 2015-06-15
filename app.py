c3db68265ccd9d210538fe386b5d1483426432e7
Ernie Hsiung <ehsiung@codeforamerica.org>
2015-06-15 15:51:07 -0700

    Split app into two screens

--- app.py
+++ app.py
@@ -11,14 +11,9 @@ db = SQLAlchemy(app)
 
 from models import Survey
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
     errors = []
-    results = {}
-
-    results = Survey.query.all()
-    print(results)
-
 
     hardcoded_api = 'https://api.typeform.com/v0/form/UYZYtI?key=433dcf9fb24804b47666bf62f83d25dbef2f629d&completed=true'
     if request.method == 'POST':
@@ -27,25 +22,10 @@ def index():
         r = requests.get(hardcoded_api)
         r_json = r.json()
 
-
         for i in r_json['responses']:
 
             try:
                 # TO DO: Add lang to models.py
-
-                '''
-                quiz_id=i['id'],
-                list_choice_1=i['answers']['list_7649733_choice'],
-                list_choice_2=i['answers']['list_7205097_choice'],
-                textfield_1=i['answers']['textfield_7205223'],
-                textarea_1=i['answers']['textarea_7205107'],
-                textarea_2=i['answers']['textarea_7205102'],
-                opinionscale_1=i['answers']['opinionscale_7205022']
-
-                print "start"
-                print "{0}, {1}, {2}, {3}, {4}, {5}, {6}".format(quiz_id, list_choice_1, list_choice_2, textfield_1, textarea_1, textarea_2, opinionscale_1)
-                print "end"
-                '''
 
                 survey = Survey(
                     lang='en',
@@ -64,6 +44,15 @@ def index():
 
             # for j in i['answers']:
             #     print j
+
+    return render_template('admin.html', errors=errors)
+
+@app.route('/')
+def index():
+    errors = []
+    results = {}
+
+    results = Survey.query.all()
 
     return render_template('index.html', errors=errors, results=results)
 
