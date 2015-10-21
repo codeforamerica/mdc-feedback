a9b76118706bfa920ffbac16044715b106c87df3
Ernie Hsiung <ehsiung@codeforamerica.org>
2015-10-21 11:48:50 -0700

    Non-roofing permits in master permit count call, #410

--- feedback/dashboard/permits.py
+++ feedback/dashboard/permits.py
@@ -19,18 +19,15 @@ VIOLATIONS_API_URL = VIOLATIONS_URL + '?$select=date_trunc_ym(ticket_created_dat
 VIOLATIONS_LOCATIONS_API_URL = VIOLATIONS_URL + '?$where=ticket_created_date_time%20%3E%20%272015-01-01%27'
 VIOLATIONS_BY_TYPE_API_URL = DATA311_URL + '?&case_owner=Regulatory_and_Economic_Resources&$select=issue_type,%20count(*)%20AS%20total&$group=issue_type&$where=ticket_created_date_time%20%3E=%20%272015-01-01%27'
 
-'''
-sophia trying to python
-'''
-    
 p_days_30 = (datetime.date.today() - datetime.timedelta(30)).strftime("%Y-%m-%d")
 p_month = datetime.datetime.now() - relativedelta(months=1)
-p_month = p_month.strftime("%Y-%m-01") 
+p_month = p_month.strftime("%Y-%m-01")
 
 VIOLATIONS_LAST_30 = VIOLATIONS_URL + '?$select=issue_type%2C%20street_address%2C%20city%2C%20ticket_status%2C%20location%2C%20method_received%2C%20ticket_last_updated_date_time%2C%20ticket_closed_date_time&$where=ticket_created_date_time%3E%27' + p_days_30 + '%27&$limit=50000'
 
 VIOLATIONS_PREV_MONTH = VIOLATIONS_URL + '?$select=issue_type%2C%20street_address%2C%20city%2C%20ticket_status%2C%20location%2C%20method_received%2C%20ticket_last_updated_date_time%2C%20ticket_closed_date_time&$where=ticket_created_date_time%3E%27' + p_month + '%27&$limit=50000'
-    
+
+
 def api_health():
     '''
     Run the API to see if its even working.
@@ -61,13 +58,14 @@ def json_to_dateobj(jsondate):
     '''
     return datetime.datetime.strptime(jsondate, '%Y-%m-%dT%H:%M:%S.000')
 
+
 @cache.memoize(timeout=86400)
 def dump_socrata_api(datatype='p'):
-  
+
     ''' For performance issues, have Sophia's AJAX calls
     get called on the server side instead.
     '''
-        
+
     data_table = {
         'p': PERMITS_API_URL,
         'v': VIOLATIONS_API_URL,
@@ -77,9 +75,9 @@ def dump_socrata_api(datatype='p'):
     }
     response = requests.get(data_table.get(datatype))
     return response.json()
-    
 
-#@cache.memoize(timeout=86400)
+
+@cache.memoize(timeout=86400)
 def lifespan_api_call(arg1=0, arg2=30, property_type='c'):
     '''
     Run the API call between arg1 days ago and arg2 days ago.
@@ -93,7 +91,7 @@ def lifespan_api_call(arg1=0, arg2=30, property_type='c'):
     days_0 = (datetime.date.today() - datetime.timedelta(arg1)).strftime("%Y-%m-%d")
     days_30 = (datetime.date.today() - datetime.timedelta(arg2)).strftime("%Y-%m-%d")
 
-    API = API_URL + '?$where=starts_with(process_number,%20%27C%27)%20AND%20master_permit_number=0%20AND%20permit_type=%27BLDG%27%20AND%20permit_issued_date%20%3E=%20%27' + days_30 + '%27%20AND%20permit_issued_date%20<%20%27' + days_0 + '%27%20AND%20'
+    API = API_URL + '?$where=category1%20not%20in(%270092%27,%20%270095%27,%20%270096%27,%20%270103%27,%20%270107%27)%20AND%20starts_with(process_number,%20%27C%27)%20AND%20master_permit_number=0%20AND%20permit_type=%27BLDG%27%20AND%20permit_issued_date%20%3E=%20%27' + days_30 + '%27%20AND%20permit_issued_date%20<%20%27' + days_0 + '%27%20AND%20'
     if property_type == 'h':
         API = API + 'contractor_name=%27OWNER%27'
     else:
@@ -115,53 +113,6 @@ def lifespan_api_call(arg1=0, arg2=30, property_type='c'):
     result1 = np.mean(lifespan_array)
 
     return result1 if not math.isnan(result1) else -1
-
-
-@cache.memoize(timeout=86400)
-def get_open_permit_lifespan():
-    '''
-    NOTE: WE ARE CURRENTLY NOT SHOWING THIS. DELETE IF NECESSARY
-    If the return value is -1 the API is down.
-    '''
-    API = API_URL + '?$select=application_date&$where=co_cc_date%20IS%20NULL%20and%20master_permit_number=0'
-    response = requests.get(API)
-    json_result = response.json()
-
-    today_str = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    today_obj = json_to_dateobj(today_str)
-
-    lifespan_array = []
-
-    for resp in json_result:
-        start_date = json_to_dateobj(resp['application_date'])
-        lifespan_array.append((today_obj-start_date).days)
-
-    return int(np.mean(lifespan_array)) if not math.isnan(np.mean(lifespan_array)) else -1
-
-
-@cache.memoize(timeout=86400)
-def get_avg_cost(property_type='c'):
-    '''
-    property_type should either be 'r', 'h' or 'c'. Defaults to 'c'.
-    Returns an integer. -1 if the API returns blank.
-    '''
-
-    API = API_URL + '?$select=AVG(permit_total_fee)&$where=starts_with(process_number,%20%27C%27)%20AND%20master_permit_number=0%20AND%20permit_type=%27BLDG%27%20AND%20'
-    if property_type == 'h':
-        API = API + 'contractor_name=%27OWNER%27'
-    else:
-        API = API + 'residential_commercial%20=%20%27' + property_type.upper() + '%27'
-
-    # Why do we care if the permit itself isn't closed?
-    # API = API + '%20and%20co_cc_date%20IS%20NULL'
-
-    response = requests.get(API)
-    result = response.json()
-    try:
-        result = result[0]['avg_permit_total_fee']
-        return result
-    except KeyError:
-        return -1
 
 
 @cache.memoize(timeout=86400)
@@ -205,7 +156,7 @@ def api_count_call(arg1=0, arg2=30, field=''):
     days_0 = (datetime.date.today() - datetime.timedelta(arg1)).strftime("%Y-%m-%d")
     days_30 = (datetime.date.today() - datetime.timedelta(arg2)).strftime("%Y-%m-%d")
 
-    API = API_URL + '?$select=count(*)%20as%20total&$where=starts_with(process_number,%20%27C%27)%20AND%20permit_type=%27BLDG%27%20AND%20master_permit_number=0%20AND%20' + field + '%20%3E%20%27' + days_30 + '%27%20AND%20' + field + '%20<%20%27' + days_0 + '%27'
+    API = API_URL + '?$select=count(*)%20as%20total&$where=category1%20not%20in(%270092%27,%20%270095%27,%20%270096%27,%20%270103%27,%20%270107%27)%20AND%20starts_with(process_number,%20%27C%27)%20AND%20permit_type=%27BLDG%27%20AND%20master_permit_number=0%20AND%20' + field + '%20%3E%20%27' + days_30 + '%27%20AND%20' + field + '%20<%20%27' + days_0 + '%27'
     # print API
     response = requests.get(API)
     json_result = response.json()
