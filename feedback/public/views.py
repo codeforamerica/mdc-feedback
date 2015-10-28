ae59255161e348fa017433a7e3159f160006cfec
phiden <sdengo@codeforamerica.org>
2015-10-28 17:01:38 -0400

    reset logged-in route.

--- feedback/public/views.py
+++ feedback/public/views.py
@@ -76,7 +76,7 @@ def auth():
 
         current_app.logger.debug(
             'LOGIN: User {} logged in successfully'.format(user.email))
-        return next_url if next_url else '/home-logged-in'
+        return next_url if next_url else '/'
 
     elif domain in current_app.config.get('CITY_DOMAINS'):
         user = User.create(email=email)
