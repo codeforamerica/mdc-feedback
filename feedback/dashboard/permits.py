f5a925782f9af5872bc6c7775a07b126af74a6e8
phiden <sdengo@codeforamerica.org>
2015-10-28 16:58:45 -0400

    reset logged in/out view

--- feedback/dashboard/permits.py
+++ feedback/dashboard/permits.py
@@ -38,7 +38,6 @@ c_month = (datetime.date.today().strftime("%Y-%m-01"))
 
 VIOLATIONS_LAST_30 = VIOLATIONS_URL + '?$select=issue_type%2C%20street_address%2C%20city%2C%20ticket_status%2C%20location%2C%20method_received%2C%20ticket_last_updated_date_time%2C%20ticket_closed_date_time&$where=ticket_created_date_time%3E%27' + c_month + '%27&$limit=50000'
 
-print VIOLATIONS_LAST_30
 
 VIOLATIONS_PREV_MONTH = VIOLATIONS_URL + '?$select=issue_type%2C%20street_address%2C%20city%2C%20ticket_status%2C%20location%2C%20method_received%2C%20ticket_last_updated_date_time%2C%20ticket_closed_date_time&$where=ticket_created_date_time%3E%27' + p_month + '%27&$limit=50000'
 
