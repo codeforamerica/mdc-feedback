7926232915401089319ee9c05d9143c7e4a25859
phiden <sdengo@codeforamerica.org>
2015-11-05 15:34:15 -0800

    debugging pdf

--- feedback/static/js/reporting.js
+++ feedback/static/js/reporting.js
@@ -1,44 +1,11 @@
 $(document).ready(function () {
   
-  console.log('hello world');
-
-/******************* HAPPY PDF-ING ********************/
+  /******************* HAPPY PDF-ING ********************/
 	
 	$('#pdf').click(function() {
   	
-  	$('#test-pdf').prepend($('#reports-view'));
-  	return xepOnline.Formatter.Format('test-pdf', {render:'download'});
+  	return xepOnline.Formatter.Format('reports-view', {render:'none', filename:'monthly_report'});
 		
   })
   
-  
-		//var appended = 0; 
-		
-		//$('#test-pdf').prepend("<h4>This is a record of your STEP information.</h4>")
-				
-   // $('#reports-view').each(function() {
-      
-      
-      
-   // })
-    
-		/*$('.module').each(function() {
-			
-			if($(this).hasClass('hidden') == false ) {
-				
-				$('#test-pdf').prepend($(this));
-				appended++;
-			}
-			
-		})
-		
-		if(appended == 0) {
-			
-			$('#test-pdf').append("It looks like we don't have any information specific to your case. ");
-		}*/
-		
-		
-			
-	
-
 });
--- feedback/static/js/xepOnline.jqPlugin.js
+++ feedback/static/js/xepOnline.jqPlugin.js
@@ -190,7 +190,7 @@ xepOnline.Formatter = {
 	getFormTextData: function(PrintCopy) {
 		var data = xepOnline.Formatter.entity_declaration + xepOnline.Formatter.xsl_stylesheet_declaration + PrintCopy;
 		//DEBUG
-		console.log(data);
+		//console.log(data);
 		var encoded = encodeURIComponent(data);
 		if(window.btoa) return btoa(encoded);
 		return encoded;
@@ -198,7 +198,8 @@ xepOnline.Formatter = {
 	getFormData: function(PrintCopy, Name, MimeType, FileName) {
 		var data = xepOnline.Formatter.entity_declaration + xepOnline.Formatter.xsl_stylesheet_declaration + PrintCopy;
 		//DEBUG
-		console.log(data);
+		//console.log(data);
+		//console.log(FileName);
 		var blob;
 		try
 		{
